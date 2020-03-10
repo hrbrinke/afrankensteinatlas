d539836dd375a34dafe060055a8388175d2d7382
Haley B <hrbrinke@iu,edu>
2020-03-10 10:56:42 -0400

    Update Galvanism and Frankenstein.php
    Added essay topic for essay 2

--- Galvanism and Frankenstein.php
+++ Galvanism and Frankenstein.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Galvanism and Frankenstein</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<p>The science in Victor Frankenstein reflects the science of Mary Shelley’s time. Luigi Galvani discovered the power of electricity on the body, and his ideas were continued by his nephew Giovanni Aldini when he attempted to bring the dead body of an executed Englishman back to life post-execution. This seems to be in direct conflict with the oft discussed Laws of Nature. This conflict is further reflected in Frankenstein’s love of nature being reduced to a husk of itself after he creates his monster. What do his actions say about morality and ethics in science at the time? Is he playing God?
+</p>
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
 
+
+<!-- Footer Section -->
+<?php include("footer.php"); 
+?>
+
+<!-- End page content -->
+</div>
+
+
+
+
+</body>
+</html>
