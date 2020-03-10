f043d300ce36b35fc5edeea00c8605b6cc03aeed
Haley B <hrbrinke@iu,edu>
2020-03-10 10:53:03 -0400

    Update Feminist Lense.php
    Added Title and Description

--- Feminist Lense.php
+++ Feminist Lense.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Women as Anchor Points </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<p>The women in Victor Frankenstein’s life represent his anchor points to normality. His mother, Justine, and, most importantly, Elizabeth are his ties to family life and his own humanity. As he loses each of these women, he loses a piece of his humanity, eventually becoming driven by revenge alone and reduced to the level of the monster that he’s created.
+</p>.
 
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
+
+
+<!-- Footer Section -->
+<?php include("footer.php"); 
+?>
+
+<!-- End page content -->
+</div>
+
+
+
+
+</body>
+</html>
